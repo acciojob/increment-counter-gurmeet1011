2648dde917601f7251671f2bfcdb7358d45482a6
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-18 18:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const countValue = document.getElementById('num');
+function increment(){
+    // get the vlue from UI
+    let value = parseInt(countValue.innerText);
+    //Update the value
+    value = value + 1;
+    //set the value onto UI
+    countValue.innerText = value;
+	alert("The current value is: " + value)
+};
